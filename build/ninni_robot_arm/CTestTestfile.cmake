cd43e563171c246a383b7007adf2b99acd539b03
NIN <ninninrapat@gmail.com>
2026-06-23 17:43:23 +0700

    remove build/install/log from tracking, add .gitignore

--- build/ninni_robot_arm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/User/Documents/Ninni_Robot_Arm/Ninni_Robor_Arm/ninni_robot_arm
-# Build directory: /mnt/c/Users/User/Documents/Ninni_Robot_Arm/Ninni_Robor_Arm/build/ninni_robot_arm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
