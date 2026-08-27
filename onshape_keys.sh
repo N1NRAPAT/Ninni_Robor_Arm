525a5413afda687e2b72d5ad28368d805ef6c405
N1NRAPAT <ninninrapat@gmail.com>
2026-08-27 19:30:30 +0700

    Untrack onshape API keys

--- onshape_keys.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export ONSHAPE_API=https://cad.onshape.com
-export ONSHAPE_ACCESS_KEY=on_MIDLEWay6gs7BNzzvmnc7
-export ONSHAPE_SECRET_KEY=7nNvB38MfV7tuaaMf8bjoLBVqcMIUaqjXdbGJlkOX0kHhhKC
